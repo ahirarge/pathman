38a0934d716f3021803e106e9e9f1e1eb52677aa
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2014-05-13 15:52:58 +0300

    Update syntax

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,83 +2,83 @@
 
 class UnitTest extends Orchestra\Testbench\TestCase {
 
-	/**
-	 * Package Providers 
-	 *
-	 * @return array
-	 */
-	protected function getPackageProviders()
+    /**
+     * Package Providers 
+     *
+     * @return array
+     */
+    protected function getPackageProviders()
     {
         return array(
-				'Barryvdh\Debugbar\ServiceProvider',
-	        	'Ahir\Pathman\PathmanServiceProvider'
-        	);
+                'Barryvdh\Debugbar\ServiceProvider',
+                'Ahir\Pathman\PathmanServiceProvider'
+            );
     }
 
-	/**
-	 * Package Aliases
-	 *
-	 * @return array
-	 */
+    /**
+     * Package Aliases
+     *
+     * @return array
+     */
     protected function getPackageAliases()
     {
         return array(
-			'Debugbar' => 'Barryvdh\Debugbar\Facade',
+            'Debugbar' => 'Barryvdh\Debugbar\Facade',
         );
     }
 
-	/**
-	 * Delete Directory
-	 *
-	 * @param  string $dir
-	 * @return boolean
-	 */
-	protected function delTree($dir) 
-	{ 
-		$files = array_diff(scandir($dir), array('.','..')); 
-		foreach ($files as $file) { 
-			(is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file"); 
-		} 
-		return rmdir($dir); 
-  	}     
+    /**
+     * Delete Directory
+     *
+     * @param  string $dir
+     * @return boolean
+     */
+    protected function delTree($dir) 
+    { 
+        $files = array_diff(scandir($dir), array('.','..')); 
+        foreach ($files as $file) { 
+            (is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file"); 
+        } 
+        return rmdir($dir); 
+    }     
 
-  	/**
-  	 * Test Create
-  	 *
-  	 * @return null
-  	 */
-  	public function testSet()
-  	{
-  		// Method is is tested
-    	$this->assertTrue(Pathman::set('sample'));
-        // Deleting test folders
-    	$this->delTree('sample');
-  	}
-
-  	/**
-  	 * Test Time Folders
-  	 *
-  	 * @return null
-  	 */
-	public function testTimeFolders()
+    /**
+     * Test Create
+     *
+     * @return null
+     */
+    public function testSet()
     {
-    	// Hashing algorithm set
-    	$hashing = Config::set('pathman::hashing', false);
-    	// Method callind
-    	$value = Pathman::timeFolders('root');
+        // Method is is tested
+        $this->assertTrue(Pathman::set('sample'));
+        // Deleting test folders
+        $this->delTree('sample');
+    }
 
-    	// True response 
-    	$required = 'root/'
-    				.date('Y').'/'
-    				.date('m').'/'
-    				.date('d').'/'
-    				.date('H').'/'
-    				.date('i').'/';
-    	// Testing
-    	$this->assertEquals($value, $required);
+    /**
+     * Test Time Folders
+     *
+     * @return null
+     */
+    public function testTimeFolders()
+    {
+        // Hashing algorithm set
+        $hashing = Config::set('pathman::hashing', false);
+        // Method callind
+        $value = Pathman::timeFolders('root');
+
+        // True response 
+        $required = 'root/'
+                    .date('Y').'/'
+                    .date('m').'/'
+                    .date('d').'/'
+                    .date('H').'/'
+                    .date('i').'/';
+        // Testing
+        $this->assertEquals($value, $required);
 
         // Deleting test folders
-    	$this->delTree('root');
+        $this->delTree('root');
     }    
 
 }
