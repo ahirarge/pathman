a3d2c88081632b44d1c8107c4df2760e997f182c
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2014-05-13 16:47:13 +0300

    Update indents

--- src/Ahir/Pathman/Pathman.php
+++ src/Ahir/Pathman/Pathman.php
@@ -5,95 +5,95 @@ use Debugbar;
 
 class Pathman {
 
-	/**
-	* Defer properties
-	*/
-	protected $defer = true;
+    /**
+    * Defer properties
+    */
+    protected $defer = true;
 
-	/**
-	* Time Folders
-	*
-	* @param  string $root
-	* @return string 
-	*/
-	public static function timeFolders($root)
-	{
-		// Checking seperation
-		if (strpos($root, '/') === false) {
-			$root.='/';
-		}
-		self::set($root);
-		// Create folders
-		$folders = explode('-', date(\Config::get('pathman::time-pattern')));
-		foreach ($folders as $key) {
-			if (\Config::get('pathman::hashing') !== false) {
-				$key = hash(\Config::get('pathman::hashing'), $key);
-			}
-			$root = $root.$key.'/';
-			self::set($root);
-		}
-		Debugbar::info('Times folders was created.');
-		return $root;
-	}	
+    /**
+    * Time Folders
+    *
+    * @param  string $root
+    * @return string 
+    */
+    public static function timeFolders($root)
+    {
+        // Checking seperation
+        if (strpos($root, '/') === false) {
+            $root.='/';
+        }
+        self::set($root);
+        // Create folders
+        $folders = explode('-', date(\Config::get('pathman::time-pattern')));
+        foreach ($folders as $key) {
+            if (\Config::get('pathman::hashing') !== false) {
+                $key = hash(\Config::get('pathman::hashing'), $key);
+            }
+            $root = $root.$key.'/';
+            self::set($root);
+        }
+        Debugbar::info('Times folders was created.');
+        return $root;
+    }   
 
-	/**
-	* Set Paths
-	*
-	* @param  string $path
-	* @return null
-	*/
-	public static function set($path)
-	{
-		try {
-			self::_create($path);
-			self::_permissions($path);
-			return true;			
-		} catch (Exception $e) {
-			throw new Exception($e->getMessage());
-		}
-	}
+    /**
+    * Set Paths
+    *
+    * @param  string $path
+    * @return null
+    */
+    public static function set($path)
+    {
+        try {
+            self::_create($path);
+            self::_permissions($path);
+            return true;            
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
 
-	/**
-	* Directory Create
-	*
-	* @param  string $path
-	* @return null
-	*/
-	protected static function _create($path)
-	{
-		# Check directory
-		if (!is_dir($path)) {
-			try {
-				$result = mkdir($path, 0777);
-			} catch (Exception $e) {
-				$message = 'Creating error: '.$path;					
-				Debugbar::error($message);
-				throw new Exception($message);				
-			}
-			Debugbar::info("Directory is created: $path");
-		}
-	}
+    /**
+    * Directory Create
+    *
+    * @param  string $path
+    * @return null
+    */
+    protected static function _create($path)
+    {
+        # Check directory
+        if (!is_dir($path)) {
+            try {
+                $result = mkdir($path, 0777);
+            } catch (Exception $e) {
+                $message = 'Creating error: '.$path;                    
+                Debugbar::error($message);
+                throw new Exception($message);              
+            }
+            Debugbar::info("Directory is created: $path");
+        }
+    }
 
-	/**
-	* Directory Permission
-	*
-	* @param  string $path
-	* @return null
-	*/
-	protected static function _permissions($path)
-	{
-		# Check permissions
-		$permission = substr(sprintf('%o', fileperms($path)), -4);
-		if ($permission != '0777') {
-			try {
-				chmod($path, 0777);
-			} catch (Exception $e) {
-				$message = 'Permission error: '.$path;
-				Debugbar::error($message);
-				throw new Exception($message);
-			}
-			Debugbar::info("Directory permissions is set: $path");
-		}
-	}
+    /**
+    * Directory Permission
+    *
+    * @param  string $path
+    * @return null
+    */
+    protected static function _permissions($path)
+    {
+        # Check permissions
+        $permission = substr(sprintf('%o', fileperms($path)), -4);
+        if ($permission != '0777') {
+            try {
+                chmod($path, 0777);
+            } catch (Exception $e) {
+                $message = 'Permission error: '.$path;
+                Debugbar::error($message);
+                throw new Exception($message);
+            }
+            Debugbar::info("Directory permissions is set: $path");
+        }
+    }
 
 }
